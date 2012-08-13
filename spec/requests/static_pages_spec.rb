03b55ab99fc65f10b01ae01c6bd689a4e40b0f5c
Yuma Uesaka <yumasax@umich.edu>
2012-08-12 20:27:17 -0700

    added more tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,18 +4,29 @@ describe "StaticPages" do
     
     describe "Home page" do
 
-        it "should have the content 'Sample App'" do
+        it "should have the h1 'Sample App'" do
             visit '/static_pages/home'
-            page.should have_content('Sample App')
+            page.should have_selector('h1', :text =>'Sample App')
+        end
+        
+        it "should have the base title" do
+            visit '/static_pages/home'
+            page.should have_selector('title',
+                                 :text => "Ruby on Rails Tutorial Sample App")
         end
 
+        it "should not have a custom page title" do
+            visit '/static_pages/home'
+            page.should_not have_selector('title', :text => "| Home")
+        end
     end
+
     
     describe "Help page" do
 
         it "should have the content 'Help'" do
             visit '/static_pages/help'
-            page.should have_content('Help')
+            page.should have_selector('h1', :text =>'Help')
         end
     end
 
@@ -27,10 +38,11 @@ describe "StaticPages" do
         end
     
 
-        it "should have the right title" do
-            visit '/static_pages/home'
+        it "should have the right title'" do
+            visit '/static_pages/about'
             page.should have_selector('title', 
-                                :text => "Ruby on Rails Tutorial Sample App | Home")
+                                :text => "| About Us")
+
         end
     end
 
